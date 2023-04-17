46084e9fed60f5189be3b5a9ab6a9f7e2d658e2a
David Ward <spy.sappin.your.sentry@gmail.com>
2023-04-16 20:16:27 -0700

    Add fact about green stars

--- src/components/About.js
+++ src/components/About.js
@@ -14,6 +14,48 @@ export default function About() {
             </a>{" "}
             of a black body entity given it's temperature.
           </p>
+        </div>
+        <div>
+          <h2>How to use this</h2>
+          <p>
+            As you use the slider or input box to modify the temperature, the
+            spectral density chart will update in real time, as will the star's
+            color.
+          </p>
+        </div>
+        <div>
+          <h2>What are the nodes? Why are some colored and others not?</h2>
+          <p>
+            Each node represents the spectral density of the black body entity
+            for each individual wavelength on the{" "}
+            <a href="https://en.wikipedia.org/wiki/Electromagnetic_radiation#Electromagnetic_spectrum">
+              electromagnetic spectrum.
+            </a>
+          </p>
+          <p>
+            There are a number of nodes on this chart, but some are colored
+            while some are transparent. The colored nodes represent a wavelength
+            that has an associated color that we humans can see, on the{" "}
+            <a href="https://www.thoughtco.com/the-visible-light-spectrum-2699036">
+              visible spectrum.
+            </a>
+            .
+          </p>
+        </div>
+        <div>
+          <h2>Wait... stars can't be green?</h2>
+          <p>Nope! At least not to the human eye.</p>
+          <p>
+            There are situations in which a star appears green, the most common
+            example of this is the{" "}
+            <a href="https://en.wikipedia.org/wiki/Green_flash">green flash</a>{" "}
+            effect, which is when refraction caused by Earth's atmosphere causes
+            the Sun's light to appear green. But the star itself is still the
+            same color, and not green.
+          </p>
+        </div>
+        <div>
+          <h2>How does it work?</h2>
           <p>
             I'm using{" "}
             <a href="https://en.wikipedia.org/wiki/Planck%27s_law#The_law">
@@ -46,33 +88,7 @@ export default function About() {
             red, but would likely just appear black to our eyes.
           </p>
         </div>
-        <div>
-          <h2>How to use this</h2>
-          <p>
-            As you use the slider or input box to modify the temperature, the
-            spectral density chart will update in real time, as will the star's
-            color.
-          </p>
-        </div>
-        <div>
-          <h2>What are the nodes? Why are some colored and others not?</h2>
-          <p>
-            Each node represents the spectral density of the black body entity
-            for each individual wavelength on the{" "}
-            <a href="https://en.wikipedia.org/wiki/Electromagnetic_radiation#Electromagnetic_spectrum">
-              electromagnetic spectrum.
-            </a>
-          </p>
-          <p>
-            There are a number of nodes on this chart, but some are colored
-            while some are transparent. The colored nodes represent a wavelength
-            that has an associated color that we humans can see, on the{" "}
-            <a href="https://www.thoughtco.com/the-visible-light-spectrum-2699036">
-              visible spectrum.
-            </a>
-            .
-          </p>
-        </div>
+
         <div>
           <h2>Popular stars to test</h2>
           <Stars />
